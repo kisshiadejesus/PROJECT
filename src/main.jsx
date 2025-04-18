eaf388e3b9f4ae788b405337bd65fa0956afc422
kisshiadejesus <dejesuskisshia18@gmail.com>
2025-04-18 20:42:00 +0800

    Your descriptive commit message here

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Login from './pages/login';
-import Welcome from './pages/welcome'; // Import with uppercase 'W' initially
+import Welcome from './pages/welcome';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/welcome" element={<Welcome />} /> {/* Keep route path as '/welcome' */}
+        <Route path="/welcome" element={<Welcome />} />
         <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
